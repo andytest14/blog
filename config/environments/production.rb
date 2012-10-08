ce533fe8ca3db59528f78e5755a71643b25bdfe1
unknown <andygriffith05@gmail.com>
2012-10-08 01:23:56 +0100

    third commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,12 +30,12 @@ Blog::Application.configure do
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
-
-  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # Eable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+   config.assets.compile = true
 
   # Enable threaded mode
   # config.threadsafe!
